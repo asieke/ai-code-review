952aebeece4cbbe3a396eabdce5f8228633a88c9
Alex Sieke <asieke@gmail.com>
2023-08-13 09:49:05 -0400

    updated octokit

--- index.js
+++ index.js
@@ -5,10 +5,16 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!  This code is gonna be off the hook`);
+  console.log(`Hello ${action}!  <<<-- This is what we're doing? >>>`);
   const time = new Date().toTimeString();
   core.setOutput('time', time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
+
+  const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
+
+  console.log(octokit);
+
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);

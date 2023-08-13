b11146133ac955edd48eea66a78a7342e05ebe64
Alex Sieke <asieke@gmail.com>
2023-08-13 15:00:08 -0400

    2023-08-13 15:00:08

--- index.js
+++ index.js
@@ -1,46 +1,51 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const { Octokit } = require('@octokit/rest');
-const fetch = require('node-fetch'); // Use require instead of import
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  const action = core.getInput('action');
-  const octokit = new Octokit({
-    auth: process.env.GITHUB_TOKEN,
-    request: { fetch },
-  });
-  const { context } = github;
+async function main() {
+  try {
+    const fetch = await import('node-fetch'); // Use dynamic import
 
-  console.log(`Hello ${nameToGreet}!  This code is gonna be off the hook`);
-  console.log(`Hello ${action}!  <<<-- This is what we're doing? >>>`);
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    const action = core.getInput('action');
+    const octokit = new Octokit({
+      auth: process.env.GITHUB_TOKEN,
+      request: { fetch },
+    });
+    const { context } = github;
 
-  if (action === 'update-push') {
-    console.log('Were going to trick out this push');
+    console.log(`Hello ${nameToGreet}!  This code is gonna be off the hook`);
+    console.log(`Hello ${action}!  <<<-- This is what we're doing? >>>`);
+
+    if (action === 'update-push') {
+      console.log('Were going to trick out this push');
+    }
+
+    if (action === 'update-change-log') {
+      console.log('Updating the change log');
+      const { owner, repo, number } = context.issue;
+      octokit.pulls
+        .get({
+          owner,
+          repo,
+          pull_number: number,
+        })
+        .then((res) => {
+          console.log('Flattened diff:', res.data.diff_url);
+        });
+      console.log('...', context);
+    }
+
+    const time = new Date().toTimeString();
+    core.setOutput('time', time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
   }
-
-  if (action === 'update-change-log') {
-    console.log('Updating the change log');
-    const { owner, repo, number } = context.issue;
-    octokit.pulls
-      .get({
-        owner,
-        repo,
-        pull_number: number,
-      })
-      .then((res) => {
-        console.log('Flattened diff:', res.data.diff_url);
-      });
-    console.log('...', context);
-  }
-
-  const time = new Date().toTimeString();
-  core.setOutput('time', time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
 }
+
+main(); // Execute the main function

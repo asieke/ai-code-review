3190998aaa2427f869114a72452b07b8b0dcfcc6
Alex Sieke <asieke@gmail.com>
2023-08-13 22:45:57 -0400

    2023-08-13 22:45:57

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ import { describePR } from './lib/describe-pr.js';
 
 async function main() {
   try {
-    const { GITHUB_TOKEN, OPENAI_API_KEY } = process.env;
     // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
     const action = core.getInput('action');
 
     if (action === 'update-push') {
@@ -55,39 +53,3 @@ async function main() {
 }
 
 main(); // Execute the main function
-
-function minimizeDiff(diffContent) {
-  const lines = diffContent.split('\n');
-  const allowedExtensions = /\.(js|ts|jsx|tsx|svelte|css|html)\b/;
-  let result = '';
-  let isDeletingFile = false;
-  let includeFile = false;
-
-  for (let i = 0; i < lines.length; i++) {
-    const line = lines[i];
-
-    // Check if the line starts a new file diff
-    if (line.startsWith('diff --git')) {
-      // Check if the file has an allowed extension
-      includeFile = allowedExtensions.test(line);
-      isDeletingFile = false;
-
-      if (includeFile) {
-        result += line + '\n';
-      }
-    } else if (includeFile) {
-      // Check for file deletion header
-      if (line.startsWith('deleted file')) {
-        isDeletingFile = true;
-        result += line + '\n';
-      }
-
-      // If not deleting, include lines until the next file diff
-      if (!isDeletingFile && !lines[i + 1]?.startsWith('diff --git')) {
-        result += line + '\n';
-      }
-    }
-  }
-
-  return result;
-}

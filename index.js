f1e00c9e1950c0133d500768322bd581ee3cccfd
Alex Sieke <asieke@gmail.com>
2023-08-13 23:01:45 -0400

    2023-08-13 23:01:45

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ import fs from 'fs';
 import { Base64 } from 'js-base64';
 
 import { describePR } from './lib/describe-pr.js';
+import { updateChangeLog } from './lib/update-change-log.js';
 
 async function main() {
   try {
     // `who-to-greet` input defined in action metadata file
     const action = core.getInput('action');
 
-    if (action === 'update-push') {
-      console.log('[ACTION] - UPDATING PUSH');
+    if (action === 'update-change-log') {
+      console.log('[ACTION] - UPDATING CHANGE LOG');
+      await updateChangeLog();
     }
 
     if (action === 'update-pr') {
